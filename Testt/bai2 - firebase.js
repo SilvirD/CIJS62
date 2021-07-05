ac2590562469d220591fdeadfe43fe34928b793f
SilvirD <bupbi99@gmail.com>
2021-07-05 22:48:31 +0700

    Midterm Test Code Intensive

--- Testt/bai2 - firebase.js
+++ Testt/bai2 - firebase.js
@@ -1,3 +1,13 @@
+// Bài toán 2 cần có 1 collection: Employee
+
+// Document trong Employee cần có các thông tin sau
+// - id
+// - Job
+// - first name
+// - last name
+// - task list
+// - employee list
+
 async function addEmployeeToFB(empInfo) {
   try {
     const employee = await firebase.firestore().collection("employee").add({
